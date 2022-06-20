ebef24f81c8c4db3e6553664504cb11b75bb32c2
gasparyanartur <gasparyanartur99@gmail.com>
2022-06-20 22:40:11 +0200

    Add wifi-cam main

--- wifi-cam/src/main.cpp
+++ wifi-cam/src/main.cpp
@@ -1,9 +1,117 @@
-#include <Arduino.h>
+#define CAMERA_MODEL_WROVER_KIT
 
-void setup() {
-  // put your setup code here, to run once:
+#include "esp_wifi.h"
+#include "esp_camera.h"
+#include "camera_pins.h"
+#include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+// Setup Access Point Credentials
+const char* NW_SSID = "Artur";
+const char* NW_PW = "23D3037900C64";
+
+
+/*
+
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+
+*/
+camera_config_t config;
+
+void startCameraServer();
+void config_init();
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  config_init();
+
+
+   
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+  WiFi.begin(NW_SSID, NW_PW);
+  Serial.printf("Connecting to WIFI: %s", NW_SSID);
+  while (!WiFi.isConnected()) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.printf("Successfully connected to address: %s\n\n", WiFi.localIP().toString());
+
+  startCameraServer();
+
+  Serial.println("Setup complete\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(10000);
+};
+
+void config_init(){
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+}
+
+void startCameraServer() {
+  // httpd_uri_t 
 }
