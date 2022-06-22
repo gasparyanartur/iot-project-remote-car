bec4c4b1f9185ba20908164e041d1661354352ea
gasparyanartur <gasparyanartur99@gmail.com>
2022-06-22 20:59:17 +0200

    Add cat server

--- web_server/src/server.py
+++ web_server/src/server.py
@@ -1,25 +1,39 @@
-class Server:
-    __slots__ = 'running'
+import websockets as ws
+import asyncio
 
-    def __init__(self) -> None:
-        self.running: bool = False
-    
-    def run(self):
-        print("Running server")
-        self.running = True
 
-        while self.running:
-            self._mainloop()
+async def handler(socket):
+    print("connected")
+    async for msg in socket:
+        print(msg)
+        if msg == "cat":
+            print("kitty time")
+            await socket.send("ok")
+
+            cat_pieces = []
+            while (chunk := await socket.recv()) != "cat done":
+                cat_pieces.append(chunk)
             
 
-    def _mainloop(self):
-        ...
+            with open("saved_cat.jpg", "wb") as new_cat_img:
+                for pcs in cat_pieces:
+                    new_cat_img.write(pcs)
 
-    
-def run_server():
-    serv = Server()
-    serv.run()
+        else:
+            print(msg)
+
+        #await socket.send(f"Hello, {name}")
+        #print(">>> hello", name)
+
+
+async def start():
+    async with ws.serve(handler, "192.168.1.104", 8001):
+        await asyncio.Future()
+
+
+def start_server():
+    asyncio.run(start())
 
 
 if __name__ == "__main__":
-    run_server()
+    start_server()
