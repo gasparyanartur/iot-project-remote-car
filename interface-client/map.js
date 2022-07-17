e363483f328a82f55a5fccae48819be4661c9268
Artur Gasparyan <gasparyanartur99@gmail.com>
2022-07-17 19:41:42 +0200

    Refactor map

--- interface-client/map.js
+++ interface-client/map.js
@@ -1,13 +1,5 @@
 const createMap = async () => {
-    const houseStats = {
-        width: 0,
-        height: 0
-    };
-
-    const areas = [];
-    const points = [];
-
-    const readSpecs = async (houseStats) => {
+    const readSpecs = async () => {
         const file = await fetch('roomspecs.json');
 
         if (file.status == 404) {
@@ -16,43 +8,41 @@ const createMap = async () => {
         }
         const data = await file.json();
 
-        console.log(data);
-
         const pixelToMeter = (pix) => {
             return pix * data.general.scale / (data.general.zoom * data.general.pix_to_cm * 100);
         };
 
-        houseStats.width = pixelToMeter(data.house.width);
-        houseStats.height = pixelToMeter(data.house.height);
+        const house = {
+            width: pixelToMeter(data.house.width),
+            height: pixelToMeter(data.house.height)
+        };
 
-        areas.splice(0, areas.length);
-        points.splice(0, points.length);
-
-        data.areas.forEach(area => {
+        const areas = data.areas.map(area => {
             const x = pixelToMeter(area.x1);
             const y = pixelToMeter(area.y1);
             const w = pixelToMeter(area.x2 - area.x1);
             const h = pixelToMeter(area.y2 - area.y1);
-            areas.push({ x, y, w, h });
+            return { x, y, w, h };
         });
 
-        data.points.forEach(pt => {
+        const points = data.points.map(pt => {
             const x = pixelToMeter(pt.x);
             const y = pixelToMeter(pt.y);
-            points.push({ x, y });
+            return { x, y };
         });
+
+        return { areas, points, house };
     };
 
-
-    await readSpecs(houseStats);
+    const specs = await readSpecs();
 
     const canvas = document.getElementById("map-canvas");
     const ctx = canvas.getContext("2d");
 
     const meterToBrowserSize = (meter) => 50 * meter;
 
-    const canvasWidth = meterToBrowserSize(houseStats.width);
-    const canvasHeight = meterToBrowserSize(houseStats.height);
+    const canvasWidth = meterToBrowserSize(specs.house.width);
+    const canvasHeight = meterToBrowserSize(specs.house.height);
 
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
@@ -61,12 +51,12 @@ const createMap = async () => {
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-        areas.forEach(w => {
+        specs.areas.forEach(w => {
             ctx.fillStyle = 'blue';
             ctx.fillRect(meterToBrowserSize(w.x), meterToBrowserSize(w.y), meterToBrowserSize(w.w), meterToBrowserSize(w.h));
         });
 
-        points.forEach(p => {
+        specs.points.forEach(p => {
             ctx.fillStyle = 'red';
             ctx.moveTo(meterToBrowserSize(p.x), meterToBrowserSize(p.y));
             console.log(meterToBrowserSize(p.x), meterToBrowserSize(p.y));
