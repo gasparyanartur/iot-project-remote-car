dd1e973a77b6f39345a32225ad9084f106c69010
gasparyanartur <gasparyanartur99@gmail.com>
2022-08-01 15:36:39 +0200

    Add motor messages

--- robot-controller/src/message_types.h
+++ robot-controller/src/message_types.h
@@ -24,25 +24,47 @@ namespace MessageHeader
         const byte Data = 1;
     }
 
+    namespace CommandType
+    {
+        const byte Move = 1;
+    }
+
+    namespace MotorSelection 
+    {
+        const byte First = 1;
+        const byte Second = 2;
+        const byte FirstAndSecond = 3;
+    }
+
+    namespace MoveDirection 
+    {
+        const byte None = 0;
+        const byte Forward = 1;
+        const byte Backward = 2;
+    }
+
     namespace StatusType
     {
         const byte OK = 1;
         const byte FAIL = 2;
     }
 
-    namespace MeasurementType {
+    namespace MeasurementType
+    {
         const byte Rotation = 1;
         const byte Acceleration = 2;
         const byte Gravity = 3;
     }
 
-    namespace RotationUnit {
+    namespace RotationUnit
+    {
         const byte Quaternions = 1;
         const byte Radians = 2;
         const byte Degrees = 3;
     }
 
-    namespace AccelerationType {
+    namespace AccelerationType
+    {
         const byte Raw = 1;
         const byte Relative = 2;
         const byte World = 3;
